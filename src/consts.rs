47780c03231bb9b09bea177119ffb3598bb8ad79
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-10-03 16:18:59 -0300

    fix window typo

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,3 @@
-pub const WINDOWN_SIZE: f32 = 800.0;
+pub const WINDOW_SIZE: f32 = 800.0;
 pub const SIZE: f32 = 25.0;
-pub const WINDOW_SIZE_HALF: f32 = WINDOWN_SIZE / 2.0;
+pub const WINDOW_SIZE_HALF: f32 = WINDOW_SIZE / 2.0;
